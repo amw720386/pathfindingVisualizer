bf4d808e683fb0707e44020a13aa5fc12d7dcd45
Ahamed Mohamed Wajibu <ahamedmohamedwajibu@gmail.com>
2024-08-08 21:13:19 -0400

    actual visualizing part added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import screeninfo
 import sys
 import heapq
+import threading
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -80,11 +81,15 @@ class Block:
         self.rendery = y*scaled_block_height
         self.border_width = border_width
         self.hover = False
+        self.path = False
         self.border_rect = pygame.Rect(self.renderx,self.rendery,scaled_block_width,scaled_block_height)
         self.inner_rect = pygame.Rect(self.renderx + self.border_width, self.rendery + self.border_width, scaled_block_width - self.border_width*2, scaled_block_height - self.border_width*2)
         self.hover_rect = pygame.Surface((scaled_block_width, scaled_block_height))
         self.hover_rect.set_alpha(128)
         self.hover_rect.fill((255,255,255))
+        self.path_rect = pygame.Surface((scaled_block_width, scaled_block_height))
+        self.path_rect.set_alpha(100)
+        self.path_rect.fill((0,0,255))
         self.pathUse = False
 
     def draw(self, screen):
@@ -93,11 +98,13 @@ class Block:
             pygame.draw.rect(screen,BLACK,self.inner_rect)
         if self.type == 2: #wall
             pygame.draw.rect(screen,GREEN,self.inner_rect)
+        if self.path: #considered in path
+            screen.blit(self.path_rect,(self.renderx, self.rendery))
+        if self.pathUse: #used in path
+            pygame.draw.rect(screen,BLUE,self.inner_rect)
         if self.type == 3: #weighted
             pygame.draw.rect(screen,GREY,self.inner_rect)
-        if self.pathUse:
-            pygame.draw.rect(screen,BLUE,self.inner_rect)
-        if self.hover:
+        if self.hover: #hovered over
             screen.blit(self.hover_rect,(self.renderx, self.rendery))
 
 def defineRatios(x,y):   
@@ -138,6 +145,11 @@ def astar(grid, start, goal):
             while current_node:
                 path.append(current_node.position)
                 current_node = current_node.parent
+
+            for position in path:
+                map.render[position[1]][position[0]].pathUse = True 
+                time.sleep(0.2)
+
             return path[::-1]  
 
         closed_list.add(current_node.position)
@@ -149,7 +161,8 @@ def astar(grid, start, goal):
                 if grid[new_position[1]][new_position[0]].type == 1:  
                     if new_position in closed_list:
                         continue
-
+                    grid[new_position[1]][new_position[0]].path = True
+                    time.sleep(0.05)
                     neighbor_node = Node(new_position, current_node)
 
                     neighbor_node.g = current_node.g + 1
@@ -158,12 +171,18 @@ def astar(grid, start, goal):
 
                     if all(neighbor_node.f < node.f for node in open_list if node.position == neighbor_node.position):
                         heapq.heappush(open_list, neighbor_node)
-
     return []  
 
-map = Map(256,144) #OUTER IS NON EXLCUSIVE I.E 512 and 288 or 256 and 144
-player = Avatar(2,2)
-goal = Goal(79,12)
+map = Map(256,144) #OUTER IS NON EXLCUSIVE I.E 512 and 288 or 256 and 144 < SET DIMENSIONS
+player = Avatar(15,2) # < SET PLAYER STARTING POSITION
+goal = Goal(134,112) # < SET GOAL POSITION
+
+if not (0 < goal.x < map.width) or not (0 < goal.y < map.height):
+    sys.exit('goal not in range')
+if not (0 < player.x < map.width) or not (0 < player.y < map.height):
+    sys.exit('player not in range')
+
+pathfinding_thread = threading.Thread(target=astar,args=(map.render, (player.x,player.y), (goal.x,goal.y)), daemon=True)
 
 while True:
     keys = pygame.key.get_pressed()
@@ -174,17 +193,13 @@ while True:
             pygame.quit()
             sys.exit()
 
-    if keys[K_z]:         
-        try:
-            for position in path:
-                map.render[position[1]][position[0]].pathUse = False 
-        except:
-            pass
-
-        path = astar(map.render, (player.x,player.y), (goal.x,goal.y))
-
-        for position in path:
-            map.render[position[1]][position[0]].pathUse = True 
+        if event.type == pygame.KEYUP:   
+            if event.key == pygame.K_z:     
+                for row in map.render:
+                    for column in row:
+                        column.path = False
+                        column.pathUse = False
+                pathfinding_thread.start()
 
     if pygame.mouse.get_pos()[0] > 0 and pygame.mouse.get_pos()[0] < screen_width:
         if pygame.mouse.get_pos()[1] > 0 and pygame.mouse.get_pos()[1] < screen_height:
@@ -203,7 +218,6 @@ while True:
 
     player.draw(screen)
     goal.draw(screen)
-
 
     pygame.display.flip()
     screen.fill(WHITE)
